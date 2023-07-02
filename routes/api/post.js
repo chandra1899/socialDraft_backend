dbebfaa020c647bae1f29adeeb991f534164495c
chandra <chandra091827@gamil.com>
2023-07-02 21:45:41 +0530

    deploy

--- routes/api/post.js
+++ routes/api/post.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const postsController=require('../../controllers/post_controller')
 const passport=require('passport')
-const formidable=require('express-formidable');
+const formidable=require('formidable');
 
 router.post('/create',passport.checkAuthentication,formidable(),postsController.create);
 router.get('/delete/:id',passport.checkAuthentication,postsController.destroy);
--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController=require('../../controllers/user_controller')
 const passport=require('passport')
-const formidable=require('express-formidable')
+const formidable=require('formidable')
 
 router.post('/create',formidable(),userController.create)
 router.post('/update',formidable(),passport.checkAuthentication,userController.update);
