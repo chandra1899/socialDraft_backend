3e60aa7024c8547963a88bfef834efdd8122017f
chandra <101719497+chandra1899@users.noreply.github.com>
2024-03-04 22:30:50 +0530

    added limit to posts in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,8 +2,11 @@ const Post=require('../models/post')
 
 module.exports.home=async (req,res)=>{
     try {
+        const { page } = req.query;
+        const pageSize = 10;
+        const skip = (page - 1) * pageSize
     let posts=await Post.find({}).select("-photo")
-    .sort('-createdAt')
+    .sort('-createdAt').skip(skip).limit(10)
     .populate({
         path:'user',
         select:'-avatar'
@@ -17,7 +20,6 @@ module.exports.home=async (req,res)=>{
             select:'-avatar'
         }
     })
-
     return res.status(200).json({posts})
         
     } catch (err) {
